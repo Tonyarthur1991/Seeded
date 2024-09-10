55619bc64878a2620fb3859c3680caa6337853ab
Tony <arthurtony1991@gmail.com>
2024-09-10 22:28:25 +0100

    Initial commit

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
@@ -147,4 +148,5 @@ def predict():
 initialize_models()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
